bcdca3ce17e7c7ca966fd9b912b451394a04ed16
Yem Daro <100412991+darororo@users.noreply.github.com>
2024-08-05 17:01:29 +0700

    use clippy

--- src/editor.rs
+++ src/editor.rs
@@ -1,4 +1,4 @@
-use std::io::{self, Read};
+use crossterm::event::{read, Event::Key, KeyCode::Char};
 use crossterm::terminal::{disable_raw_mode, enable_raw_mode};
 
 pub struct Editor {
@@ -10,25 +10,25 @@ impl Editor {
         Editor{}
     }
 
+
     pub fn run(&self) {
-    enable_raw_mode().unwrap();
-        for b in io::stdin().bytes() {
-            match b {
-                Ok(b) => {
-                    let c = b as char;
-                    if c.is_control() {
-                        println!("Binary: {0:08b} ASCII: {0:#03} \r", b);
-                    } else {
-                        println!("Binary: {0:08b} ASCII: {0:#03} Character: {1:#?}\r", b, c);
-                    }
-                    if c == 'q' {
-                        disable_raw_mode().unwrap();
-                        break;
+        enable_raw_mode().unwrap();
+        loop {
+            match read() {
+                Ok(Key(event)) => {
+                    println!("{event:?} \r");
+
+                    if let Char(c) = event.code {
+                        if c == 'q' {
+                            break;
+                        }
                     }
                 }
-                Err(err) => println!("Error {}", err),
+
+                Err(err) => println!("Error: {err}"),
+                _ => ()
             }
-            disable_raw_mode().unwrap();
-    }
+        }
+        disable_raw_mode().unwrap();
     }
 }
